45473d6f0555ac921e9dfa5af5a385afbd46dd83
Konrad Krueger <KonKrueger@gmail.com>
2022-06-25 21:59:03 +0200

    the haskell boss has entered the room

---
+++ src/sexier.hs
@@ -0,0 +1,6 @@
+tanga = ["lol", "No Way", "dafaq", "befehl\nlol", "prove\n"]
+untanga = "lol\nNo Way\ndafaq\n"
+testString = "PREFIX dbo: <http://dbpedia.org/ontology/> PREFIX res: <http://dbpedia.org/resource/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+
+removeN xs = [ x | x <- xs, not (x `elem` "\n")]
+
